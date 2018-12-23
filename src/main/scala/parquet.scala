bf0076965d06af7f8d31dfc9b06f8b1f7d04e4b4
meitei11 <prabin.meitei@gmail.com>
2018-12-23 22:11:56 +0530

    changes for successful working

--- src/main/scala/parquet.scala
+++ src/main/scala/parquet.scala
@@ -1,24 +1,32 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 
 
 object parquet {
 
   def main(args: Array[String]) {
-    val conf:SparkConf = new SparkConf()
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val inputFile = "/Users/prabin/work/spark/test.json"
+    val outputFile = "/Users/prabin/work/spark/out.parquet"
+    val conf:SparkConf = new SparkConf().setSparkHome("/usr/local/spark")
+      .setAppName("First App").setMaster("local")
+    val spark = SparkSession
+      .builder()
+      .appName("First")
+      .config(conf)
+      .getOrCreate()
+
+    convertToParquet(spark, inputFile, outputFile)
     spark.stop()
   }
 
-  def convertToParquet(sc:SparkContext, sql:SQLContext, inputFile:String, outputFile:String): Unit ={
-    println(s"Reading JSON from: $inputFile and writing to: $outputFile")
-    val df = sc.read.json(inputFile)
-    df.save(outputFile, "parquet")
+  def convertToParquet(spark:SparkSession, inputFile:String, outputFile:String)= {
+    println("===================================================================================")
+    println(s"Reading JSON from: $inputFile and writing to: $outputFile ")
+    val df = spark.read.json(inputFile)
+    df.printSchema()
+    println("===================================================================================")
+
+    df.write.parquet(outputFile)
   }
 
 }
